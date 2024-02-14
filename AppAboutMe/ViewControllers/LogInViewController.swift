ce89ff58b08a23c9ccad05961796d2f35d386d27
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-14 04:13:32 +0300

    Adjusting the appearance of the LogInVC

--- AppAboutMe/ViewControllers/LogInViewController.swift
+++ AppAboutMe/ViewControllers/LogInViewController.swift
@@ -11,11 +11,27 @@ class LogInViewController: UIViewController {
     
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var logInButton: UIButton!
+    
+    let gradientLogInView = CAGradientLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInButton.layer.cornerRadius = 5
+        
+        gradientLogInView.frame = view.bounds
+        gradientLogInView.colors = [UIColor.systemBlue.cgColor, UIColor.systemMint.cgColor, UIColor.systemIndigo.cgColor]
+        gradientLogInView.locations = [0.0, 0.5, 1.0]
+        view.layer.insertSublayer(gradientLogInView, at: 0)
+        
+        userNameTextField.textColor = .black
+        userNameTextField.attributedPlaceholder = NSAttributedString(string: "User Name", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+        passwordTextField.textColor = .black
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+       
+        
     }
 
     @IBAction func pressedLogInButton() {
